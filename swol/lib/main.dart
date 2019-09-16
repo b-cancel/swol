9cf6c7e68a1708617403b24468abc84be9630cc4
Bryan Cancel <bryan.o.cancel@gmail.com>
2019-09-15 21:28:46 -0500

    icon and splash screen setup... moving onto list of excercise

--- swol/lib/main.dart
+++ swol/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:swol/excercise.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,155 +7,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SWOL Equation Test',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+      title: 'SWOL',
+      home: ExcerciseList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class ExcerciseList extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ExcerciseListState createState() => _ExcerciseListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  //control
-  TextEditingController controlWeight = new TextEditingController();
-  int weight;
-  TextEditingController controlReps = new TextEditingController();
-  int reps;
-
-  //functions
-  String onlyCharactersS2E(String string, int startInc, int endInc){
-    String output = "";
-    for(int i = 0; i < string.length; i++){
-      int code = string[i].codeUnitAt(0);
-      if(startInc <= code && code <= endInc){
-        output += string[i];
-      }
-    }
-    return output;
-  }
-
-  String onlyNumbers(String string){
-    return onlyCharactersS2E(string, 48, 57);
-  }
-
-  String onlyCharacters(String string){
-    return onlyCharactersS2E(string, 97, 122);
-  }
-
-  int strToInt(String str){
-    String justNumbers = onlyNumbers(str);
-    if(justNumbers == null || justNumbers.length == 0){
-      return 0;
-    }
-    else return int.parse(justNumbers);
-  }
-
-  //init
+class _ExcerciseListState extends State<ExcerciseList> {
   @override
   void initState() {
-    //inits
-    weight = 0;
-    reps = 0;
-
-    //listen to changes
-    controlWeight.addListener((){
-      weight = strToInt(controlWeight.text);
-      setState(() {
-        
-      });
-    });
-
-    controlReps.addListener((){
-      reps = strToInt(controlReps.text);
-      setState(() {
-        
-      });
+    //auto navigate to excercise widget
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context){
+            return Excercise();
+          },
+        ),
+      );
     });
 
     //super init
     super.initState();
   }
 
-  //build
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Swol Equation Testing"),
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              //control
-              Row(children: <Widget>[
-                TextField(
-                  controller: controlWeight,
-                  keyboardType: TextInputType.number,
-                ),
-                TextField(
-                  controller: controlReps,
-                  keyboardType: TextInputType.number,
-                )
-              ],)
-              //equations
-              //TODO: add equation drop down
-              //TODO: add slider between 1 and 35 reps
-            ],
-          ),
-        ),
+    return Container(
+      child: Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
 }
-
-/*
-1 Brzycki Function
-2 McGlothin (or Landers) Function
-3 Almazan Function *our own*
-4 Epley (or Baechle) Function
-5 O`Conner Function
-6 Wathan Function
-7 Mayhew Function
-8 Lombardi Function
-*/
-
-/*
-% of max 1 Rep Max
-1 ->
-25 ->
-50 ->
-75 ->
-100
-Function Links
-
-muscular tension 
-0%
-20%
-20%
-60%
-90%
-https://www.desmos.com/calculator/ajcgllfbbo
-
-muscular damage
-10%
-20%
-60%
-20%
-10%
-https://www.desmos.com/calculator/yf2tl8pq3b
-
-metabolic stress
-90%
-60%
-20%
-20%
-0%
-https://www.desmos.com/calculator/tkwifmbtpi
-*/
