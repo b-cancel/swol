7e6480d1c8600c6ffe7dc9021aead1cbd4876461
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-02-18 13:40:53 -0600

    slightly edited the set record for calibration...only slightly because realistically adding a sliver can backfire on smaller screens... solve with shimering arrows

--- swol/lib/action/tabs/record/setRecord.dart
+++ swol/lib/action/tabs/record/setRecord.dart
@@ -1,5 +1,7 @@
 //flutter
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:swol/action/popUps/error.dart';
 
 //internal: action
@@ -63,16 +65,28 @@ class SetRecord extends StatelessWidget {
     bool lastSetOrBefore = setsPassed <= excercise.setTarget.value;
     Color buttonsColor =  lastSetOrBefore ? Theme.of(context).accentColor : Theme.of(context).cardColor;
 
-    //clipping so "hero" doesn't show up in the other page
-    return ClipRRect(
-      child: ListView(
-        children: <Widget>[
-          Container(
-            height: spaceToRedistribute,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Conditional(
+    //widget
+    Widget recordSetFields = CardWithHeader(
+      header: "Record Set",
+      aLittleSmaller: true,
+      child: RecordFields(
+        heroAnimDuration: heroAnimDuration,
+      ),
+    );
+
+    Widget buttonsOnBottom = Theme(
+      data: MyTheme.light,
+      child: SetRecordButtonsWithWhiteContext(
+        buttonsColor: buttonsColor, 
+        excercise: excercise, 
+        timerNotStarted: timerNotStarted, 
+        backAction: backAction,
+      ),
+    );
+
+    if(calibrationRequired){
+      /*
+      Conditional(
                   condition: calibrationRequired,
                   ifTrue: Padding(
                     padding: EdgeInsets.only(
@@ -80,7 +94,63 @@ class SetRecord extends StatelessWidget {
                     ),
                     child: CalibrationCard(),
                   ),
-                  ifFalse: Expanded(
+                  ifFalse: 
+                ),
+      */
+
+      /*
+      Visibility(
+                  visible: calibrationRequired,
+                  child: 
+                ),
+      */
+      //clipping so "hero" doesn't show up in the other page
+      return ClipRRect(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: spaceToRedistribute,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  CalibrationCard(),
+                  Container(
+                    height: 48.0,
+                    child:  Shimmer.fromColors(
+                      direction: ShimmerDirection.ltr,
+                      baseColor: Theme.of(context).primaryColor,
+                      highlightColor: Theme.of(context).cardColor,
+                      child: Icon(
+                        MaterialCommunityIcons.getIconData("chevron-double-up"),
+                        size: 36,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                  recordSetFields,
+                  Expanded(
+                    child: Container()
+                  ),
+                  buttonsOnBottom,
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    else{
+      //clipping so "hero" doesn't show up in the other page
+      return ClipRRect(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: spaceToRedistribute,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
                     child: MakeFunctionAdjustment(
                       heroUp: heroUp,
                       heroAnimDuration: heroAnimDuration,
@@ -88,35 +158,17 @@ class SetRecord extends StatelessWidget {
                       excercise: excercise,
                     ),
                   ),
-                ),
-                CardWithHeader(
-                  header: "Record Set",
-                  aLittleSmaller: true,
-                  child: RecordFields(
-                    heroAnimDuration: heroAnimDuration,
-                  ),
-                ),
-                Visibility(
-                  visible: calibrationRequired,
-                  child: Expanded(
-                    child: Container()
-                  ),
-                ),
-                Theme(
-                  data: MyTheme.light,
-                  child: SetRecordButtonsWithWhiteContext(
-                    buttonsColor: buttonsColor, 
-                    excercise: excercise, 
-                    timerNotStarted: timerNotStarted, 
-                    backAction: backAction,
-                  ),
-                ),
-              ],
+                  recordSetFields,
+                  buttonsOnBottom,
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
+
+    
   }
 }
 
