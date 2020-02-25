b99529ff8b5f9e4a2bdcefad6f9aa7a19c7a6137
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-02-25 15:45:27 -0600

    sorting deleting duplicates... will have to go back and make sure this isnt breaking anythign anywhere and provide a general case solution

--- swol/lib/action/shared/toFunctionOrder.dart
+++ swol/lib/action/shared/toFunctionOrder.dart
@@ -1,6 +1,8 @@
 import 'package:swol/action/page.dart';
 
 updateOrderOfIDs(List<double> functionIDToValue){
+  print(functionIDToValue.toString() + " S");
+
   //recalculate all the potential values for function order
   Map<double,int> valueToFunctionID = new Map<double,int>();
   for(int functionID = 0; functionID < 8; functionID++){
@@ -11,14 +13,18 @@ updateOrderOfIDs(List<double> functionIDToValue){
   //grab the keys and sort them
   List<double> values = valueToFunctionID.keys.toList();
   values.sort((b, a) => a.compareTo(b)); //largest to smallest
+
+  print(values.toString() + " M");
+
   List<int> orderedIDs = new List<int>(8);
   for(int i = 0; i < values.length; i++){
     double value = values[i];
     int id = valueToFunctionID[value];
+    print(value.toString() + " => " + id.toString());
     orderedIDs[i] = id;
   }
 
-  print(orderedIDs.toString());
+  print(orderedIDs.toString() + " E");
   
   //set the value so all notifies get notified
   ExcercisePage.orderedIDs.value = orderedIDs;
