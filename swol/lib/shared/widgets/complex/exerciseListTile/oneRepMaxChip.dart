889954ce3e5499b2072c951c510fdf59a086e521
Bryan Cancel <bryan.o.cancel@gmail.com>
2021-06-30 16:04:19 -0400

    edited chips to not show percents since they are confusing

--- swol/lib/shared/widgets/complex/exerciseListTile/oneRepMaxChip.dart
+++ swol/lib/shared/widgets/complex/exerciseListTile/oneRepMaxChip.dart
@@ -6,7 +6,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 //internal
 import 'package:swol/shared/widgets/simple/oneOrTheOtherIcon.dart';
-import 'package:swol/shared/widgets/simple/ourSnackBar.dart';
 import 'package:swol/shared/structs/anExercise.dart';
 import 'package:swol/other/functions/helper.dart';
 
@@ -85,7 +84,6 @@ class _ExerciseTileSubtitleState extends State<ExerciseTileSubtitle> {
         widget.exercise.lastWeight.toString(),
         style: BOLD,
       );
-      String snackbarMessage = "Your 1 Rep Max";
 
       //only calculate the 1 rep max and all its related values if you must
       if (widget.exercise.lastReps != 1) {
@@ -109,107 +107,49 @@ class _ExerciseTileSubtitleState extends State<ExerciseTileSubtitle> {
 
         double standardDevaition = oneRepMaxValues[2];
 
-        if (standardDevaition.toInt() == 0) {
-          snackbarMessage = "\"Without a doubt\" this is your 1 Rep Max";
-          //NOTE: we keep default valueBorderColor
+        //we aren't sure about our result
+        //NOTE: updating
+        //1. oneRepMaxWidget
+        //2. snackBarMessage
+        //3. valueBorderColor
+
+        //make that clear by
+        //1. showing the devaition
+        //2. but also by highligting the GUESS in an appropiate color
+        //3. and add an appropiate snackbar with
+        //    a. sureness string
+        //    b. suggestions to improve
+        double mean = oneRepMaxValues[1];
+        int deviation;
+        if (mean.toInt() == 0 || standardDevaition.toInt() == 0) {
+          deviation = 0;
         } else {
-          //we aren't sure about our result
-          //NOTE: updating
-          //1. oneRepMaxWidget
-          //2. snackBarMessage
-          //3. valueBorderColor
-
-          //make that clear by
-          //1. showing the devaition
-          //2. but also by highligting the GUESS in an appropiate color
-          //3. and add an appropiate snackbar with
-          //    a. sureness string
-          //    b. suggestions to improve
-          double mean = oneRepMaxValues[1];
-          int percentOfDeviation;
-          if (mean.toInt() == 0 || standardDevaition.toInt() == 0) {
-            percentOfDeviation = 0;
-          } else {
-            percentOfDeviation = ((standardDevaition / mean) * 100).toInt();
-          }
-
-          //"Map" percent devaition to all its respective components
-          //red, orange, yellow, green, blue, purple
-          String surenessString;
-          int lessThanReps;
-          if (percentOfDeviation > 25) {
-            surenessString = "not sure at all";
-            lessThanReps = 25;
-          } else if (percentOfDeviation > 20) {
-            surenessString = "very un-sure";
-            lessThanReps = 20;
-          } else if (percentOfDeviation > 15) {
-            surenessString = "somewhat un-sure";
-            lessThanReps = 15;
-          } else if (percentOfDeviation > 10) {
-            surenessString = "somewhat sure";
-            lessThanReps = 10;
-          } else if (percentOfDeviation > 5) {
-            surenessString = "very sure";
-            lessThanReps = 5;
-          } else {
-            surenessString = "extremely sure";
-            lessThanReps = 0;
-          }
-
-          //let the user know how far the guess is
-          snackbarMessage =
-              "We are \"" + surenessString + "\" this is your one rep max";
-
-          //ONLY discourage anything above 15
-          //since anything below is going to produce pretty good results
-          snackbarMessage += (lessThanReps <= 15)
-              ? ""
-              : "\nDo less than " +
-                  lessThanReps.toString() +
-                  " reps for a more accurate guess";
-
-          //we are here because we have some level of uncertainty so we need to display that
-          oneRepMaxWidget = Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              oneRepMaxWidget,
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 2.0,
-                ),
-                child: OneOrTheOtherIcon(
-                  one: Icon(
-                    FontAwesomeIcons.plus,
-                    color: Colors.white.withOpacity(0.75),
-                  ),
-                  other: Icon(
-                    FontAwesomeIcons.minus,
-                    color: Colors.white.withOpacity(0.75),
-                  ),
-                ),
-              ),
-              Text(
-                percentOfDeviation.toInt().toString() + "%",
-              ),
-            ],
-          );
+          deviation = standardDevaition.toInt();
+          // ((standardDevaition / mean) * 100).toInt();
         }
 
-        //generate the bordered widget
+        //we are here because we have some level of uncertainty so we need to display that
         oneRepMaxWidget = Row(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             oneRepMaxWidget,
             Padding(
-              padding: const EdgeInsets.only(
-                left: 4.0,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 2.0,
               ),
-              child: Icon(
-                Icons.info_outline,
-                color: Colors.white.withOpacity(0.50),
-                size: 16,
+              child: OneOrTheOtherIcon(
+                one: Icon(
+                  FontAwesomeIcons.plus,
+                  color: Colors.white.withOpacity(0.75),
+                ),
+                other: Icon(
+                  FontAwesomeIcons.minus,
+                  color: Colors.white.withOpacity(0.75),
+                ),
               ),
+            ),
+            Text(
+              deviation.toInt().toString(),
             ),
           ],
         );
@@ -220,70 +160,62 @@ class _ExerciseTileSubtitleState extends State<ExerciseTileSubtitle> {
       return Row(
         mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              openSnackBar(
-                context,
-                Colors.blue,
-                Icons.info_outline,
-                message: snackbarMessage,
-              );
-            },
-            //NOTE: this is just extra padding to make it easier to click the chip
-            child: Container(
-              child: Padding(
-                padding: EdgeInsets.only(
-                  top: 2.0,
-                  right: 16.0,
-                  bottom: 4.0,
-                ),
-                //NOTE: can join rounded edges but not border with rounded edges
-                //which is why those are seperate below
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    //-------------------------1 RM tag
-                    Container(
-                      decoration: BoxDecoration(
+          Container(
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 2.0,
+                right: 16.0,
+                bottom: 4.0,
+              ),
+              //NOTE: can join rounded edges but not border with rounded edges
+              //which is why those are seperate below
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  //-------------------------1 RM tag
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColorDark,
+                      borderRadius: new BorderRadius.only(
+                        topLeft: Radius.circular(borderRadius),
+                        bottomLeft: Radius.circular(borderRadius),
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 4,
+                      vertical: 5, //1 pixel to account for border width
+                    ),
+                    child: Text(
+                      (isOneRepMaxEstimated ? "E-" : "") + "1RM",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  //-------------------------Value
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        //default width of 1.0
                         color: Theme.of(context).primaryColorDark,
-                        borderRadius: new BorderRadius.only(
-                          topLeft: Radius.circular(borderRadius),
-                          bottomLeft: Radius.circular(borderRadius),
-                        ),
                       ),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 4,
-                        vertical: 5, //1 pixel to account for border width
-                      ),
-                      child: Text(
-                        (isOneRepMaxEstimated ? "E-" : "") + "1RM",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+                      borderRadius: new BorderRadius.only(
+                        bottomRight: Radius.circular(borderRadius),
+                        topRight: Radius.circular(borderRadius),
                       ),
                     ),
-                    //-------------------------Value
-                    Container(
-                      padding: EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          //default width of 1.0
-                          color: Theme.of(context).primaryColorDark,
-                        ),
-                        borderRadius: new BorderRadius.only(
-                          bottomRight: Radius.circular(borderRadius),
-                          topRight: Radius.circular(borderRadius),
-                        ),
-                      ),
-                      child: oneRepMaxWidget,
-                    ),
-                  ],
-                ),
+                    child: oneRepMaxWidget,
+                  ),
+                ],
               ),
             ),
           ),
-          Expanded(child: Container(color: Colors.blue)),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
         ],
       );
     }
